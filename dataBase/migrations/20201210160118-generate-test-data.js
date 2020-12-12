b90d5647950bb648059bf0597061188c986d67a3
NatalieKy <nataliekyryk1@gmail.com>
2020-12-12 20:02:11 +0200

    password hashing for test data added

--- dataBase/migrations/20201210160118-generate-test-data.js
+++ dataBase/migrations/20201210160118-generate-test-data.js
@@ -4,7 +4,7 @@ module.exports = {
     async up(queryInterface) {
         await queryInterface.bulkInsert('students', [
             { name: 'Viktor', age: 26, gender: 'male', email: 'viktor@gmail.com', password: await passwordHasher('Qwerty123') },
-            { name: 'IronBird', age: 24, gender: 'female', email: 'ironbird@gmail.com', password: await passwordHasher('Qwerty321') },
+            { name: 'IronBird', age: 24, gender: 'female', email: 'ironbird@gmail.com', password: await passwordHasher('Qwerty333') },
             { name: 'Olga', age: 25, gender: 'female', email: 'olya@gmail.com', password: await passwordHasher('Qwerty123') },
         ]);
         await queryInterface.bulkInsert('cars', [
