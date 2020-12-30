029d91a0f252c9bf3718e35fe3fee239ad7df631
NatalieKy <nataliekyryk1@gmail.com>
2020-12-30 20:00:46 +0200

    fixed

--- Controllers/car/cars.controllers.js
+++ Controllers/car/cars.controllers.js
@@ -21,53 +21,45 @@ module.exports = {
             const newCar = await carService.createCar(car, transaction);
 
             if (photos) {
-                try {
-                    // eslint-disable-next-line max-len
-                    const photosPathWithoutPublic = path.join('users', `${student_id}`, `car ${newCar.dataValues.id}`, 'car photos');
-                    const photosFullPath = path.join(process.cwd(), 'public', photosPathWithoutPublic);
+                // eslint-disable-next-line max-len
+                const photosPathWithoutPublic = path.join('users', `${student_id}`, `car ${newCar.dataValues.id}`, 'car photos');
+                const photosFullPath = path.join(process.cwd(), 'public', photosPathWithoutPublic);
 
-                    await fs.mkdir(photosFullPath, { recursive: true });
+                await fs.mkdir(photosFullPath, { recursive: true });
 
-                    photos.map(async (photo) => {
-                        const photoExtension = photo.name.split('.').pop();
-                        const newPhotoName = `${uuid.v1()}.${photoExtension}`;
+                photos.map(async (photo) => {
+                    const photoExtension = photo.name.split('.').pop();
+                    const newPhotoName = `${uuid.v1()}.${photoExtension}`;
 
-                        await photo.mv(path.join(photosFullPath, newPhotoName));
+                    await photo.mv(path.join(photosFullPath, newPhotoName));
 
-                        const file_type = PHOTO_TYPE;
-                        const file_path = await path.join(photosFullPath, newPhotoName);
+                    const file_type = PHOTO_TYPE;
+                    const file_path = await path.join(photosFullPath, newPhotoName);
 
-                        await carService.updateSingleCarPhotos({ file_type, file_path }, newCar.dataValues.id, transaction);
-                        await transaction.commit();
-                    });
-                } catch (e) {
-                    await transaction.rollback();
-                }
+                    await carService.updateSingleCarPhotos({ file_type, file_path }, newCar.dataValues.id, transaction);
+                    await transaction.commit();
+                });
             }
 
             if (documents) {
-                try {
-                    // eslint-disable-next-line max-len
-                    const documentsPathWithoutPublic = path.join('users', `${student_id}`, `car ${newCar.dataValues.id}`, 'car documents');
-                    const documentsFullPath = path.join(process.cwd(), 'public', documentsPathWithoutPublic);
+                // eslint-disable-next-line max-len
+                const documentsPathWithoutPublic = path.join('users', `${student_id}`, `car ${newCar.dataValues.id}`, 'car documents');
+                const documentsFullPath = path.join(process.cwd(), 'public', documentsPathWithoutPublic);
 
-                    await fs.mkdir(documentsFullPath, { recursive: true });
+                await fs.mkdir(documentsFullPath, { recursive: true });
 
-                    documents.map(async (document) => {
-                        const photoExtension = document.name.split('.').pop();
-                        const newDocumentName = `${uuid.v1()}.${photoExtension}`;
+                documents.map(async (document) => {
+                    const photoExtension = document.name.split('.').pop();
+                    const newDocumentName = `${uuid.v1()}.${photoExtension}`;
 
-                        await document.mv(path.join(documentsFullPath, newDocumentName));
+                    await document.mv(path.join(documentsFullPath, newDocumentName));
 
-                        const file_type = DOCUMENT_TYPE;
-                        const file_path = await path.join(documentsFullPath, newDocumentName);
+                    const file_type = DOCUMENT_TYPE;
+                    const file_path = await path.join(documentsFullPath, newDocumentName);
 
-                        await carService.updateSingleCarDocuments({ file_type, file_path }, newCar.dataValues.id, transaction);
-                        await transaction.commit();
-                    });
-                } catch (e) {
-                    await transaction.rollback();
-                }
+                    await carService.updateSingleCarDocuments({ file_type, file_path }, newCar.dataValues.id, transaction);
+                    await transaction.commit();
+                });
             }
 
             res.status(CREATED).json(newCar);
@@ -108,7 +100,6 @@ module.exports = {
             await carService.updateSingleCar(dataToUpdate, car_id, transaction);
 
             if (photos) {
-                try {
                     const photosPathWithoutPublic = path.join('users', `${student_id}`, `car ${car_id}`, 'car photos');
                     const photosFullPath = path.join(process.cwd(), 'public', photosPathWithoutPublic);
 
@@ -126,9 +117,6 @@ module.exports = {
                         await carService.updateSingleCarPhotos({ file_type, file_path }, car_id, transaction);
                         await transaction.commit();
                     });
-                } catch (e) {
-                    await transaction.rollback();
-                }
             }
 
             if (documents) {
